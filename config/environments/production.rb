a029b942e9279725338e68320114e859c04ab65b
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-02-24 09:19:54 +0200

    use api-key for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,12 +120,12 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   ActionMailer::Base.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port => "587",
-    :authentication => :plain,
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
+    :user_name => "apikey",
+    :password => ENV["SENDGRID_API_KEY"],
     :domain => "heroku.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
     :enable_starttls_auto => true,
   }
 
